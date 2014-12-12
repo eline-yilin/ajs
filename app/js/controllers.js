f93d36f3150feb8ff9d6648fb92421908fc6d297
AndyZheng <tianhua.zheng@theoutnet.cn>
2014-12-12 17:23:49 +0800

    'ajs-controller'

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,3 +10,57 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+angular.module('myApp.controllers', []).
+controller('driversController', function($scope, ciAPIservice) {
+	$scope.nameFilter = null;
+	$scope.driversList = [];
+	$scope.searchFilter = function (driver) {
+	    var keyword = new RegExp($scope.nameFilter, 'i');
+	    return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+	};
+	
+	ciAPIservice.getDrivers().success(function (response) {
+        //Dig into the responde to get the relevant data
+        //$scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+    $scope.driversList = [
+      {
+          Driver: {
+              givenName: 'Sebastian',
+              familyName: 'Vettel',
+              driverId: 1
+          },
+          points: 322,
+          nationality: "German",
+          Constructors: [
+              {name: "Red Bull"}
+          ]
+      },
+      {
+          Driver: {
+          givenName: 'Fernando',
+              familyName: 'Alonso',
+              driverId: 2
+          },
+          points: 207,
+          nationality: "Spanish",
+          Constructors: [
+              {name: "Ferrari"}
+          ]
+      }
+    ];
+}).
+controller('driverController', function($scope, $routeParams, ciAPIservice) {
+    $scope.id = $routeParams.id;
+    $scope.races = [];
+    $scope.driver = null;
+
+    ciAPIservice.getDriverDetails($scope.id).success(function (response) {
+        $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]; 
+    });
+
+    ciAPIservice.getDriverRaces($scope.id).success(function (response) {
+        $scope.races = response.MRData.RaceTable.Races; 
+    }); 
+  });
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,30 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
+  factory('ciAPIservice', function($http) {
+
+    var restAPI = {};
+
+    restAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+    restAPI.getDriverDetails = function(id) {
+        return $http({
+          method: 'JSONP', 
+          url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+        });
+      }
+
+    restAPI.getDriverRaces = function(id) {
+        return $http({
+          method: 'JSONP', 
+          url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+        });
+      }
+
+    return restAPI;
+  }).
   value('version', '0.1');
